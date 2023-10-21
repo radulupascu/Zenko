bc781ed03c132b9b670a6c33ccd7f5b6f28222d4
razwanel <razwanelul@gmail.com>
2023-10-21 23:52:53 +0300

    ;

--- Pinecone/main.py
+++ Pinecone/main.py
@@ -1,20 +1,34 @@
 import pinecone as pc
 import pandas as pd
+from langchain.document_loaders.csv_loader import CSVLoader
+
+
+loader = CSVLoader(file_path='../data/FDV_TRANSN_updated.csv', csv_args={
+    'delimiter': ',',
+    'quotechar': '"',
+
+
+
+})
+data = loader.load()
+
+print(data)
  
 # initialize connection to pc (get API key at app.pc.io)
 pc.init(
     api_key=open("../P_KEY", "r").read(),
     environment="gcp-starter"  # find next to API key in console
 )
-#pc.create_index('database1', dimension=768)
-#print(pc.list_indexes())
-pc.create_index('test', dimension=2)
-index=pc.Index('test')
+# pc.create_index('database1', dimension=1536)
+# print(pc.list_indexes())
+index_name = "database1"
+if index_name not in pc.list_indexes():
+    pc.create_index(index_name=index_name, dimension=1536, metric='euclidian')
 
-index.upsert([
-    ('Pisica', [0. , 0.]),
-    ('Caine', [0. , 0.])
-])
+# pc.create_index('test', dimension=2)
+#index=pc.Index(index_name)
+
+
 
 #df = pd.read_csv('../data/FDV_TRANSN_updated.csv')
 #df.dropna(inplace=True)
